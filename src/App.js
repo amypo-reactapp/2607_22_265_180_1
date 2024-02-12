71d88b7bac770a455bb60830d8b2c7c900c7c22a
amypo-reactapp <reactapp@amypo.in>
2024-02-12 09:05:38 +0000

    User

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I am nalla senathipathi good boy       
         </a>
       </header>
     </div>
